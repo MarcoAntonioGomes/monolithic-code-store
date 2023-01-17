dc27cc8e34aff18c0cdcff68c4a459f3d5f137f9
marco <marco@joinder.me>
2023-01-16 22:05:02 -0300

    developed product-adm module

---
+++ src/modules/product-adm/facade/product-adm.facade.ts
@@ -0,0 +1,31 @@
+import UseCaseInterface from "../../@shared/domain/usecase/use-case.interface";
+import ProductAdmFacadeInterface, {
+  AddProductFacadeInputDto,
+  CheckStockFacadeInputDto,
+  CheckStockFacadeOutputDto,
+} from "./product-adm.facade.interface";
+
+export interface UserCasesProps {
+  addUseCase: UseCaseInterface;
+  stockUseCase: UseCaseInterface;
+}
+
+export default class ProductAdmFacade implements ProductAdmFacadeInterface {
+  private _addUseCase: UseCaseInterface;
+  private _checkStockUseCase: UseCaseInterface;
+
+  constructor(useCasesProps: UserCasesProps) {
+    this._addUseCase = useCasesProps.addUseCase;
+    this._checkStockUseCase = useCasesProps.stockUseCase;
+  }
+
+  addProduct(input: AddProductFacadeInputDto): Promise<void> {
+    return this._addUseCase.execute(input);
+  }
+
+  checkStock(
+    input: CheckStockFacadeInputDto
+  ): Promise<CheckStockFacadeOutputDto> {
+    return this._checkStockUseCase.execute(input);
+  }
+}
