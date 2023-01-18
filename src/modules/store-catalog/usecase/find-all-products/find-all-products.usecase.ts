098d177275f317f08dadecd0e140b50c681f4d22
marco <marco@joinder.me>
2023-01-18 08:31:03 -0300

    developed store-catalog  module

---
+++ src/modules/store-catalog/usecase/find-all-products/find-all-products.usecase.ts
@@ -0,0 +1,20 @@
+import UseCaseInterface from "../../../@shared/domain/usecase/use-case.interface";
+import ProductGateway from "../../gateway/product.gateway";
+
+export default class FindAllProductsUseCase implements UseCaseInterface {
+  constructor(private productRepository: ProductGateway) {}
+
+  async execute(): Promise<any> {
+    const products = await this.productRepository.findAll();
+    return {
+      products: products.map((product) => {
+        return {
+          id: product.id.id,
+          name: product.name,
+          description: product.description,
+          salesPrice: product.salesPrice,
+        };
+      }),
+    };
+  }
+}
